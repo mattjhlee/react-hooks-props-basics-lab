b9a4b24e4e8f4dc220f0dbb049ca685351760ee2
Matthew <mattjhlee25@gmail.com>
2023-05-23 10:36:17 -0400

    fix about

--- src/components/About.js
+++ src/components/About.js
@@ -2,13 +2,10 @@ import React from "react";
 import Links from "./Links.js"
 
 function About(props) {
-  if(!props.bio || props.bio.trim() === "") {
-    return null;
-  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{props.bio}</p>
+      {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links 
         github={props.github}
